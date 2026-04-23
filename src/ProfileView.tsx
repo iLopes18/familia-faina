d4b471e8926c941bd55a1eb416f81705d52d7346
iLopes18 <18isaaclopes@gmail.com>
2026-04-23 12:12:47 +0100

    Update ProfileView.tsx

--- src/ProfileView.tsx
+++ src/ProfileView.tsx
@@ -25,11 +25,10 @@ import { AvatarConfig } from "./types";
 import { handleFirestoreError, OperationType } from "./utils/firebaseErrors";
 
 const banners = [
-  "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?auto=format&fit=crop&q=80&w=1000",
-  "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?auto=format&fit=crop&q=80&w=1000",
-  "https://images.unsplash.com/photo-1501854140801-50d01698950b?auto=format&fit=crop&q=80&w=1000",
-  "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?auto=format&fit=crop&q=80&w=1000",
-  "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?auto=format&fit=crop&q=80&w=1000",
+  "/banner1.png",
+  "/banner2.png", // Adicione os outros nomes conforme estão na pasta public
+  "/banner3.png",
+  "/banner4.png",
 ];
 
 import { calculateMatriculaYear } from "./utils/dateUtils";
