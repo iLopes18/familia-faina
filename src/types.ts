0169142733070e815f231297f2de5fc1c6f53989
iLopes18 <18isaaclopes@gmail.com>
2026-05-04 17:21:24 +0100

    Add optional ano_conclusao property to types

--- src/types.ts
+++ src/types.ts
@@ -12,6 +12,7 @@ export interface Member {
   manualExtension?: number; // in pixels
   curso_faina?: string;
   curso_atual?: string;
+  ano_conclusao?: number | null;
   bannerUrl?: string;
   order?: number;
   avatar?: AvatarConfig;
